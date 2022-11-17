2d56c662a525cf4ad96d76d09dd29f3d8c47137b
PIAAR <piaar@rmasters.group>
2022-11-17 10:24:37 -0500

    Main web commited

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import './js/components/home/Homepage.component.scss'
+
+import App from './App'
+import Home from './js/components/home/Homepage.component.jsx'
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
+      <Home />
   </React.StrictMode>
 );
 
---
+++ src/js/components/home/Homepage.component.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            HEllo Yalwww!! This is the home page
+        </div>
+    )
+}
+
+export default Home;
